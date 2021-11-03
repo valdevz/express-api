44d67cc7fe024c88346107c3f018a0f9e9a21035
jv_h2d <jorge.valdez@h2d.app>
2021-11-03 12:01:09 -0600

    first commit from dev

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/', function(req, res) {
+    res.send('hello holaaa holaaaa');
+  });
